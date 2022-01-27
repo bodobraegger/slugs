93c4b19e6a1be125763875cea852aca12d485fd0
= <braegger.bodo@gmail.com>
2022-01-27 19:50:52 +0100

    better intro

--- Source/slugs/js/narration.js
+++ Source/slugs/js/narration.js
@@ -10,18 +10,18 @@ class Narration {
         startNewLogSegment()
         logOutput(`well done! this is how you can interact with the game, by typing in commands in the box below. when you start typing, suggestions will appear.`)
         logOutput(`to help make it a bit quicker, there are a few tricks: when you press the tab ⭾ or the right arrow ➡️, the first suggestion will be typed out for you. when you press the left arrow ⬅️, it will be deleted again. the up and down arrow ⬆️⬇️ keys cycle through the suggestions if there are multiple, or to cycle through the last commands if there aren't any suggestions.`)
-        logOutput(`when you type something that the game does not understand, the box will shake a little, but don't worry, that's just to help you notice! it might also shake if a message is repeated, or there is another reason why you should look at this part of the game.`)
+        logError(`when you type something that the game does not understand, the box will shake a little, but don't worry, that's just to help you notice! it might also shake if a message is repeated, or there is another reason why you should look at this part of the game.`)
+        logOutput(`the last way you can interact with the game is by dragging and pinching the beings in the world by clicking on them if you need to.`) //you can change what you see on the left using the ⬅️⬆️⬇️➡️ arrow keys,
         logOutput(`once you have read this, type ${wrapCmd('intro 2')} to learn your objective!`)
     }
     intro_2() {
         logOutput(`great work!`)
-        logOutput(`the last way you can interact with the game is by dragging and pinching the beings in the world by clicking on them if you need to.`) //you can change what you see on the left using the ⬅️⬆️⬇️➡️ arrow keys,
         logOutput(`your goal is to enable your being to grow! it is not very clever on its own. you should communicate with it using commands and tell it how to grow properly by commanding it to ${wrapCmd('eat')}.`)
         logOutput(`whenever the being is close to fruit it thinks it could ${wrapCmd('eat')}, it will show you by <u class='beingscolor'>projecting a line to the fruit</u> :). other beings might do the same! when you leave the being alone for a while, it will do things on its own...`)
         logOutput(`it grows by eating fruit that's good for it - if it is hurt or eats fruit that is bad for it, it will become seethrough until it can find some good fruit again. to figure out what the being likes, it will have to try interacting with the world. to find out what you can commands you can use to talk to the being, you can enter the ${wrapCmd('help')} command.`)
 
         logOutput(`if you ever want to see the introduction again, you now know the commands to view it! (hint: ${wrapCmd('intro')} and a number for the section).`)
-        logOutput(`congratulations! you now know how to play the game. have fun! if you don't know what to do, are confused, or need inspiration, try using the ${wrapCmd('help')} command!`)
+        logOutput(`congratulations! you now know how to play the game. have fun! if you don't know what to do, are confused, or need inspiration, try using the ${wrapCmd('help')} command! to learn more about your being, try the ${wrapCmd(showWord)} command.`)
         if(wordsToCompare.indexOf('intro') != -1) wordsToCompare.splice(wordsToCompare.indexOf('intro'), 1);
     }
     loopNudge() {
