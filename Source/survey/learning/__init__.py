3f2c61aa8564ea7821cd84851908c6a8beda89fa
= <braegger.bodo@gmail.com>
2022-02-04 00:50:30 +0100

    section 3

--- Source/survey/learning/__init__.py
+++ Source/survey/learning/__init__.py
@@ -23,7 +23,7 @@ class Group(BaseGroup):
 
 
     
-def generate_label_div(choices): 
+def generate_imgs_div(choices): 
     r = '<div style="display: flex">'
     for x in choices:
         r+=f'''<div style="flex: 1; text-align: center">
@@ -34,34 +34,41 @@ def generate_label_div(choices):
     return r+ '</div>'
 def generate_img_tag(filename):
     return f"<img src='/static/{filename}.png'>"
+def generate_pres_div(choices):
+    r = '<div>'
+    for i, x in enumerate(choices):
+        r+=f'''<div class='code' style='width: fit-content'>
+                {string.ascii_uppercase[i]}. {x}
+            </div>'''
+    return r+ '</div>'
 class Player(BasePlayer):
     # section 1
-    fruit_not_fit = models.StringField(widget=widgets.RadioSelectHorizontal, choices=['B', 'C', 'D', 'E', 'H'], blank=True, label='Please select the fruit that does not fit into the below Set.' + generate_label_div(['B', 'C', 'D', 'E', 'H']))
-    fruit_combine = models.LongStringField(blank=True, label='Please combine the following fruit into sets, by writing the corresponding letters of a combination a line. Always include all the fruits involved in a combination. Add a one word description next to the set that explains why the fruit belong together.<br>' + generate_label_div(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']) + f"Incomplete example: B, C; red")
+    fruit_not_fit = models.StringField(widget=widgets.RadioSelectHorizontal, choices=['B', 'C', 'D', 'E', 'H'], blank=True, label='Please select the fruit that does not fit into the below Set.' + generate_imgs_div(['B', 'C', 'D', 'E', 'H']))
+    fruit_combine = models.LongStringField(blank=True, label='Please combine the following fruit into sets, by writing the corresponding letters of a combination a line. Always include all the fruits involved in a combination. Add a one word description next to the set that explains why the fruit belong together.<br>' + generate_imgs_div(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']) + f"Incomplete example: B, C; red")
     fruit_instruction_change = models.StringField(blank=True, 
         label=f'''The selection below was created using this instruction: “Select the green fruit.”<br>
-            {generate_label_div(['A', 'D', 'E', 'G'])}<br>
+            {generate_imgs_div(['A', 'D', 'E', 'G'])}<br>
             How should the instruction be changed in order to generate the selection below?<br>
-            {generate_label_div(['A', 'E', 'F', 'G'])}''')
-    leaf_combine = models.LongStringField(blank=True, label='Please combine the following leaves into sets, by writing the corresponding letters of a combination a line. Always include all the leaves involved in a combination. Add a one word description next to the set that explains why the leaves belong together.<br>' + generate_label_div(['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']))
-    leaf_correct_instructions_1 = models.StringField(blank=True, label=f'Please select the correct instructions to help someone produce the selection  {generate_label_div(["I", "J"])} from the leaves: {generate_label_div(list(string.ascii_uppercase)[8:16])}', choices=['1. Select all leaves that are spiky.', '2. Select all leaves that are round and smooth.', '3. Select all leaves that have 7 segments.', '4. Select all leaves that are darker.',])
-    leaf_correct_instructions_2 = models.StringField(blank=True, label=f'Please select the correct instructions to help someone produce the selection {generate_label_div(["O", "N", "L", "K"])} from the leaves: {generate_label_div(list(string.ascii_uppercase)[8:16])}', choices=['1. Select all leaves that are spiky.', '2. Select all leaves that are round and smooth.', '3. Select all leaves that have 7 segments.', '4. Select all leaves that are lighter.',])
+            {generate_imgs_div(['A', 'E', 'F', 'G'])}''')
+    leaf_combine = models.LongStringField(blank=True, label='Please combine the following leaves into sets, by writing the corresponding letters of a combination a line. Always include all the leaves involved in a combination. Add a one word description next to the set that explains why the leaves belong together.<br>' + generate_imgs_div(['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']))
+    leaf_correct_instructions_1 = models.StringField(blank=True, label=f'Please select the correct instructions to help someone produce the selection  {generate_imgs_div(["I", "J"])} from the leaves: {generate_imgs_div(list(string.ascii_uppercase)[8:16])}', choices=['1. Select all leaves that are spiky.', '2. Select all leaves that are round and smooth.', '3. Select all leaves that have 7 segments.', '4. Select all leaves that are darker.',])
+    leaf_correct_instructions_2 = models.StringField(blank=True, label=f'Please select the correct instructions to help someone produce the selection {generate_imgs_div(["O", "N", "L", "K"])} from the leaves: {generate_imgs_div(list(string.ascii_uppercase)[8:16])}', choices=['1. Select all leaves that are spiky.', '2. Select all leaves that are round and smooth.', '3. Select all leaves that have 7 segments.', '4. Select all leaves that are lighter.',])
     leaf_instruction_change = models.StringField(blank=True, 
         label=f'''The selection below was created using this instruction: “Select all leaves with 7 segments.”<br>
-            {generate_label_div(['M', 'P'])}<br>
+            {generate_imgs_div(['M', 'P'])}<br>
             How should the instruction be changed in order to generate the selection below?<br>
-            {generate_label_div(['K', 'L', 'M', 'N', 'O', 'P'])}''')
+            {generate_imgs_div(['K', 'L', 'M', 'N', 'O', 'P'])}''')
     # section 2
     shampoo = models.StringField(blank=True, 
         label='''Zorg, an alien from a planet where everything is understood literally, is visiting earth. Zorg
             bought a shampoo at the supermarket so she can wash her antennae. As she tries it in the
             shower, she can’t seem to finish showering at all because she follows the instructions on the
             bottle exactly. The instructions read:<br>
-            <code>
+            <pre>
             Wash<br>
             Rinse<br>
             Repeat<br>
-            </code>
+            </pre>
             <img src="/static/shampoo.png">
             <p>Add or remove a single instruction so she can successfully shower:</p>
             ''')
@@ -83,44 +90,38 @@ class Player(BasePlayer):
             <p>Suggest a way of making this process easier for the people on the farm.</p>
             ''')
     # section 3
-    js_ifelse = models.IntegerField(blank=True, 
-        label='''In the following JavaScript snippet, there is a line missing. Select the answers to fill in the
-            missing line that would make it correct and let it log the following output to the console: <code>you
-            need to practice debugging</code>.<br>
-            JavaScript Snippet: <br>
-            <code>
-                let goal = 'computational thinking'
-                let activity = 'unsure'
-                if(goal == 'fastest 100m sprint')
-                {
-                    activity = 'running'
-                }
-                // MISSING LINE
-                {
-                    activity = 'debugging'
-                }
-                console.log('you need to practice ' + activity)
-            </code>'''
-        , choices=["if(goal == 'computational thinking')", "else if(goal == 'fastest 100m sprint')", "else", "if goal is computational thinking then"])
-    js_while = models.IntegerField(blank=True, 
-        label='''In the following JavaScript snippet, there is a line missing. Select the answers to fill in the
-            missing line that would make it correct and let it log the following output to the console: <code>you
-            need to practice debugging</code>.<br>
-            JavaScript Snippet: <br>
-            <code>
-                let goal = 'computational thinking'
-                let activity = 'unsure'
-                if(goal == 'fastest 100m sprint')
-                {
-                    activity = 'running'
-                }
-                // MISSING LINE
-                {
-                    activity = 'debugging'
-                }
-                console.log('you need to practice ' + activity)
-            </code>'''
-        , choices=["if(goal == 'computational thinking')", "else if(goal == 'fastest 100m sprint')", "else", "if goal is computational thinking then"])
+    js_ifelse = models.StringField(blank=True, widget=widgets.RadioSelect,
+        label='''
+In the following JavaScript snippet, there is a line missing. Select the answers to fill in the
+missing line that would make it correct and let it log the following output to the console: <code>you
+need to practice debugging</code>.<br>
+JavaScript Snippet: <br>
+<div class='code'><pre style="margin: 0; line-height: 125%"><span style="color: #007020; font-weight: bold">let</span> goal <span style="color: #666666">=</span> <span style="color: #4070a0">&#39;computational thinking&#39;</span>
+<span style="color: #007020; font-weight: bold">let</span> activity <span style="color: #666666">=</span> <span style="color: #4070a0">&#39;unsure&#39;</span>
+<span style="color: #007020; font-weight: bold">if</span>(goal <span style="color: #666666">==</span> <span style="color: #4070a0">&#39;fastest 100m sprint&#39;</span>) 
+{
+activity <span style="color: #666666">=</span> <span style="color: #4070a0">&#39;running&#39;</span>
+} 
+<span style="color: #60a0b0; font-style: italic">// MISSING LINE</span>
+{
+activity <span style="color: #666666">=</span> <span style="color: #4070a0">&#39;debugging&#39;</span>
+}
+console.log(<span style="color: #4070a0">&#39;you need to practice &#39;</span> <span style="color: #666666">+</span> activity)
+</pre></div>
+''' # + generate_pres_div(["if(goal == 'computational thinking')", "else if(goal == 'fastest 100m sprint')", "else", "if goal is computational thinking then"])
+, choices=["if(goal == 'computational thinking')", "else if(goal == 'fastest 100m sprint')", "else", "if goal is computational thinking then"])
+
+
+    js_while = models.StringField(blank=True, widget=widgets.RadioSelect,
+        label='''In the following boxes are snippets of JavaScript pre. Please select the snippets that produce the following output in the console: <br>
+<code>
+You are great!<br>
+You are nice!<br>
+You are clever!<br>
+</code>
+<br>
+            ''', 
+choices=['A', 'B', 'C', 'D'])
     js_fizzbuzz = models.LongStringField(blank=True, 
         label='''
             Please write a JavaScript function that, for a given input number n, returns the sum of all
@@ -129,11 +130,14 @@ class Player(BasePlayer):
             <code>%</code> operator to get the remainder of a division, for example: <code>4 % 3 = 1</code>, <code>4 % 4 = 0</code>. If the result
             is zero, then the first number is divisible by the second one.<br>
             Here is an outline for a function containing the structure of a <code>while</code> loop.<br>
-            <code> <br>
-            function bigSum(n) { <br>
-            &nbsp&nbsp&nbsp&nbsp while() <br>
-            } <br>
-            </code> <br>''')\
+<div class='code'><pre style="margin: 0; line-height: 125%"><span style="color: #007020; font-weight: bold">function</span> bigSum(n) {
+<span style="color: #007020; font-weight: bold">while</span>( )
+  {
+  <span style="color: #60a0b0; font-style: italic">// fill in the function</span>
+  }
+}
+</pre></div>
+<br>''')
 
 
 class PageInherit(Page):
@@ -151,7 +155,7 @@ class Section2(Section1):
     def vars_for_template(player):
         return {'section_number': 2}
 
-class Section3(Section1):
+class Section3(PageInherit):
     form_fields = ['js_ifelse', 'js_while', 'js_fizzbuzz']
     def is_displayed(player):
         return player.subsession.session.config.get('include_js_test') == True
